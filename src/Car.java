48a591c0063bb0e82d02a2fd86d3905418095163
Tj-Tech <aktepetuncay@gmail.com>
2019-11-17 16:42:23 -0500

    for phone branch

--- src/Car.java
+++ src/Car.java
@@ -1,6 +1,14 @@
 public class Car {
     public int Year;
     public String color;
+    public int speed;
+    public int wheel;
+    public int door;
+
+
+    public void drive(){
+
+    }public void isFast(){}
 
 
     }
